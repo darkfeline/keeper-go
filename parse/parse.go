06d4926a5287e4a0bf1512a20f5aa20bf73d8eee
Allen Li <darkfeline@felesatra.moe>
2019-10-27 21:23:27 -0700

    Write doc

--- parse/parse.go
+++ parse/parse.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package parse implements parsing of keeper entries.
 package parse
 
 import (
