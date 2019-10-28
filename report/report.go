d81c5375397fac83f1ae1dca5ae5c0606497efec
Allen Li <darkfeline@felesatra.moe>
2019-10-28 00:16:44 -0700

    Add report package

---
+++ report/report.go
@@ -0,0 +1,60 @@
+// Copyright (C) 2019  Allen Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package report implements convenient functions for processing
+// transactions and making reports.
+//
+// All functions assume that transactions are sorted and balanced.
+package report
+
+import (
+	"sort"
+
+	"cloud.google.com/go/civil"
+	"go.felesatra.moe/keeper/book"
+)
+
+func TallyBalances(ts []book.Transaction) map[book.Account]book.Balance {
+	m := make(map[book.Account]book.Balance)
+	for _, t := range ts {
+		for _, s := range t.Splits {
+			b := m[s.Account]
+			m[s.Account] = b.Add(s.Amount)
+		}
+	}
+	return m
+}
+
+func SortAccounts(as []book.Account) {
+	sort.Slice(as, func(i, j int) bool { return as[i] < as[j] })
+}
+
+func TxStarting(ts []book.Transaction, d civil.Date) []book.Transaction {
+	i := sort.Search(len(ts), func(i int) bool {
+		return !ts[i].Date.Before(d)
+	})
+	new := make([]book.Transaction, len(ts)-i)
+	copy(new, ts[i:])
+	return new
+}
+
+func TxEnding(ts []book.Transaction, d civil.Date) []book.Transaction {
+	i := sort.Search(len(ts), func(i int) bool {
+		return !ts[len(ts)-1-i].Date.After(d)
+	})
+	n := len(ts) - i
+	new := make([]book.Transaction, n)
+	copy(new, ts[:n])
+	return new
+}
---
+++ report/report_test.go
@@ -0,0 +1,203 @@
+// Copyright (C) 2019  Allen Li
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package report
+
+import (
+	"testing"
+
+	"cloud.google.com/go/civil"
+	"github.com/google/go-cmp/cmp"
+	"go.felesatra.moe/keeper/book"
+)
+
+func TestTxStarting(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		desc string
+		ts   []book.Transaction
+		d    civil.Date
+		want []book.Transaction
+	}{
+		{
+			desc: "happy",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 6},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+		},
+		{
+			desc: "empty",
+			ts:   []book.Transaction{},
+			d:    civil.Date{2001, 5, 6},
+			want: []book.Transaction{},
+		},
+		{
+			desc: "beginning",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 5},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+		},
+		{
+			desc: "past beginning",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 4},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+		},
+		{
+			desc: "end",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 7},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+		},
+		{
+			desc: "past end",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d:    civil.Date{2001, 5, 8},
+			want: []book.Transaction{},
+		},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.desc, func(t *testing.T) {
+			t.Parallel()
+			got := TxStarting(c.ts, c.d)
+			if diff := cmp.Diff(c.want, got); diff != "" {
+				t.Errorf("tx mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestTxEnding(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		desc string
+		ts   []book.Transaction
+		d    civil.Date
+		want []book.Transaction
+	}{
+		{
+			desc: "happy",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 6},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+			},
+		},
+		{
+			desc: "empty",
+			ts:   []book.Transaction{},
+			d:    civil.Date{2001, 5, 6},
+			want: []book.Transaction{},
+		},
+		{
+			desc: "beginning",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 5},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+			},
+		},
+		{
+			desc: "past beginning",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d:    civil.Date{2001, 5, 4},
+			want: []book.Transaction{},
+		},
+		{
+			desc: "end",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 7},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+		},
+		{
+			desc: "past end",
+			ts: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+			d: civil.Date{2001, 5, 8},
+			want: []book.Transaction{
+				{Date: civil.Date{2001, 5, 5}, Description: "foo"},
+				{Date: civil.Date{2001, 5, 6}, Description: "bar"},
+				{Date: civil.Date{2001, 5, 7}, Description: "baz"},
+			},
+		},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.desc, func(t *testing.T) {
+			t.Parallel()
+			got := TxEnding(c.ts, c.d)
+			if diff := cmp.Diff(c.want, got); diff != "" {
+				t.Errorf("tx mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
