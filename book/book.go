ace7f1f5dd4aa89be74183c6b671deb1b498afed
Allen Li <darkfeline@felesatra.moe>
2020-04-18 00:54:13 -0700

    Fix package comment

--- book/book.go
+++ book/book.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package book provides an accessible API for working with keeper files.
+// Package book implements bookkeeping with keeper files.
 package book
 
 import (
