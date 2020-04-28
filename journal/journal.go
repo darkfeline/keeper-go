8ef6c831a8153b876251b08ac750212142334920
Allen Li <darkfeline@felesatra.moe>
2020-04-28 01:49:45 -0700

    Update journal doc

--- journal/journal.go
+++ journal/journal.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package journal implements bookkeeping with keeper files.
+// Package journal implements bookkeeping journals with keeper files.
 package journal
 
 import (
