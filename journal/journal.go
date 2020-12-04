9b0865f04839060ae5008a3c834fbc490946b88e
Allen Li <darkfeline@felesatra.moe>
2020-12-03 20:48:57 -0800

    Fix comment

--- journal/journal.go
+++ journal/journal.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package journal implements bookkeeping journals with keeper files.
+// Package journal implements the processing of bookkeeping journals from kpr files.
 package journal
 
 import (
