55aca8c767c58b4029d1e5321ba5c9a5d943531d
Allen Li <darkfeline@felesatra.moe>
2023-10-12 22:21:46 -0700

    Add package comments

--- internal/webui/handler.go
+++ internal/webui/handler.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package webui implements the Web UI for keeper.
 package webui
 
 import (
--- internal/webui/templates/templates.go
+++ internal/webui/templates/templates.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package templates contains templates for the web UI.
+// Package templates contains templates for the Web UI.
 package templates
 
 import (
